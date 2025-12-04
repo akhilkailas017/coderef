5f2ff6aaf282091150ab1589132bcb6a75792cb1
Akhil Kailas <akhilkailas2001@gmail.com>
2025-12-04 16:38:56 +0530

    Enhance compression middleware with Brotli support
    Added Brotli compression and custom filter for specific content types.

--- prod-js-starter/middleware/compressor.js
+++ prod-js-starter/middleware/compressor.js
@@ -2,5 +2,26 @@ const compression = require('compression');
 
 module.exports = compression({
   level: 6,
-  threshold: 1024
+  threshold: 1024,
+  brotli: {
+    enabled: true,
+    zlib: {}
+  },
+
+  filter: (req, res) => {
+    const contentType = res.getHeader('Content-Type');
+    if (
+      contentType &&
+      (contentType.includes('image/') ||
+        contentType.includes('video/') ||
+        contentType.includes('audio/') ||
+        contentType.includes('zip') ||
+        contentType.includes('gzip') ||
+        contentType.includes('application/pdf'))
+    ) {
+      return false;
+    }
+
+    return compression.filter(req, res);
+  }
 });
