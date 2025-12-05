203d6e4e7404957b6aa8795dc7b5cea6cb706a84
Akhil Kailas <akhilkailas2001@gmail.com>
2025-12-05 15:47:14 +0530

    Delete prod-js-starter/swagger/swaggerLoader.js

--- prod-js-starter/swagger/swaggerLoader.js
+++
@@ -1,8 +0,0 @@
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-
-function loadSwagger(app) {
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-}
-
-module.exports = loadSwagger;
